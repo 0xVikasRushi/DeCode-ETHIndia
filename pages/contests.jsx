b13cee7a0a96e538c2914fa00d1b6562aadcd850
Shubh Chudasama <shubh.cs@proton.me>
2022-12-04 05:57:13 +0530

    Style contests.jsx

--- pages/contests.jsx
+++ pages/contests.jsx
@@ -10,61 +10,46 @@ const Contests = () => {
       .catch((error) => console.log("error", error));
   }, []);
 
+  console.log(data);
   return (
-    <div>
+    <div className="text-slate-50 m-4">
       <Head>
         <title>Contests - DeCode</title>
       </Head>
+      <p className="text-3xl mb-2">Contests</p>
+
       <div className="flex flex-col">
         <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
             <div className="overflow-hidden">
-              <table className="min-w-full text-center">
-                <thead className="border-b bg-gray-800">
+              <table className="min-w-full">
+                <thead className="bg-white border-b">
                   <tr>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-white px-6 py-4"
-                    >
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       #
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-white px-6 py-4"
-                    ></th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-white px-6 py-4"
-                    >
-                      Start Timings
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Name
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-white px-6 py-4"
-                    >
-                      End Time
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Start time
+                    </th>
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      End time
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-                  {data?.contests?.map(({ name, startTime, endTime }, i) => {
-                    return (
-                      <tr className="bg-white border-b">
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {i + 1}
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                          <Link href={"/contests/" + name}>{name}</Link>
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                          {new Date(startTime).toString()}
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                          {new Date(endTime).toString()}
-                        </td>
-                      </tr>
-                    );
-                  })}
+                  {data?.contests?.map(({ name, startTime, endTime }, idx) => (
+                    <tr key={idx} className={`${idx % 2 == 0 ? "bg-gray-100" : "bg-white"} border-b`}>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{idx + 1}</td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <Link href={`/contests/${name}`}>{name}</Link>
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{startTime}</td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{endTime}</td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
